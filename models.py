2d89d09c7e6db6987f66fc47f06d0abd47569f2d
sugandv21 <sdvsuganya@gmail.com>
2025-08-14 12:02:22 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 from extensions import db
-from datetime import date
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     is_done = db.Column(db.Boolean, default=False)
     due_date = db.Column(db.Date, nullable=False)
+
+    def __repr__(self):
+        return f"<Task {self.title}>"
