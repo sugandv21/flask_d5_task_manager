0fc60bb43fba4386ee20337f8949a5d52a30ba42
sugandv21 <sdvsuganya@gmail.com>
2025-08-14 12:01:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,22 @@
+# import os
+
+# BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# class Config:
+#     MYSQL_USER = "root"
+#     MYSQL_PASSWORD = "sugan21"
+#     MYSQL_HOST = "localhost"      
+#     MYSQL_DB = "task_db"
+
+#     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SECRET_KEY = "supersecretkey"
+
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "sugan21"
-    MYSQL_HOST = "localhost"      
-    MYSQL_DB = "task_db"
-
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'tasks.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "supersecretkey"
-
